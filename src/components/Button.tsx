d553efa68aa7fbdd8d9ae856ef074869c045a439
Joe-Mwangi <joem99271@gmail.com>
2024-06-05 12:01:07 +0300

    step1 complete

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,13 @@
+import { Button } from "./ui/button";
+
 const StepsButton = () => {
-  return <div className="flex bg-white shadow-lg fle">StepsButton</div>;
+  return (
+    <div className="flex bg-white shadow-lg md:shadow-none p-4 md:p-0  justify-between">
+      <Button variant="ghost" className="font-bold text-cool-gray">
+        Go Back
+      </Button>
+      <Button className="font-bold">Next Step</Button>
+    </div>
+  );
 };
 export default StepsButton;
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,11 @@
+import StepsButton from "./Button";
 import FormCard from "./Form";
 
 const MainContainer = () => {
   return (
-    <div className="md:px-20  md:py-8 bg-light-blue md:bg-white md:w-3/4 h-full">
+    <div className="md:px-20 flex flex-col justify-end md:py-8 bg-light-blue md:bg-white md:w-3/4 h-full md:flex md:flex-col md:justify-between">
       <FormCard />
+      <StepsButton />
     </div>
   );
 };
