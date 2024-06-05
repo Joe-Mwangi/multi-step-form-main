0c4e83b197a749b63e06cc5e1af345c007c5d963
Joe-Mwangi <joem99271@gmail.com>
2024-06-05 14:20:39 +0300

    step2

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,10 +1,12 @@
 import StepsButton from "./Button";
-import Step1 from "./steps/Step1";
+// import Step1 from "./steps/Step1";
+import Step2 from "./steps/Step2";
 
 const MainContainer = () => {
   return (
     <div className="md:px-20 flex flex-col justify-end md:py-8 bg-light-blue md:bg-white md:w-3/4 h-full md:flex md:flex-col md:justify-between">
-      <Step1 />
+      {/* <Step1 /> */}
+      <Step2 />
       <StepsButton />
     </div>
   );
