d0c172aef9e69ac52e89e0bf698b216841a276de
Joe-Mwangi <joem99271@gmail.com>
2024-06-06 11:59:06 +0300

    zustund store

---
+++ src/components/hooks/use-steps-store.tsx
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+
+interface StepsStore {
+  step: number;
+  isCompleted: boolean;
+  yearly: boolean;
+  plan: Plan;
+  addOns: Plan;
+  incrementStep: () => void;
+  decrementStep: () => void;
+  updatePlan: (plan: Plan) => void;
+  updateAddOns: (plan: Plan) => void;
+  updateYearly: () => void;
+}
+
+interface Plan {
+  title: string;
+  price: string;
+}
+
+export const useStepsStore = create<StepsStore>((set) => ({
+  step: 1,
+  isCompleted: false,
+  yearly: false,
+  plan: { title: "", price: "" },
+  addOns: { title: "", price: "" },
+  incrementStep: () => set((state) => ({ step: state.step + 1 })),
+  decrementStep: () => set((state) => ({ step: state.step - 1 })),
+  updatePlan: (plan) => set({ plan }),
+  updateAddOns: (addOns) => set({ addOns }),
+  updateYearly: () => set((state) => ({ yearly: !state.yearly })),
+}));
