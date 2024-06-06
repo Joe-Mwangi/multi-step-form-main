689891bb502d8037b5e80da64218a18a93e3a826
Joe-Mwangi <joem99271@gmail.com>
2024-06-06 03:33:32 +0300

    h

--- src/components/steps/Final.tsx
+++ src/components/steps/Final.tsx
@@ -1,6 +1,6 @@
 const FinalStep = () => {
   return (
-    <div className="absolute md:static top-32 inset-x-5  rounded-lg shadow-lg md:shadow-none md:rounded-none p-6 bg-white md:p-0">
+    <div className="absolute md:static top-32 inset-x-5 md:h-full  rounded-lg shadow-lg md:shadow-none md:rounded-none p-6 bg-white md:p-0">
       <div className="flex flex-col justify-center items-center gap-4 h-80 px-4 md:px-0 md:h-full">
         <div className="w-12 h-12  bg-[url('/images/icon-thank-you.svg')] bg-center bg-cover bg-no-repeat"></div>
         <h2 className="font-bold text-2xl text-marine-blue">Thank you!</h2>
