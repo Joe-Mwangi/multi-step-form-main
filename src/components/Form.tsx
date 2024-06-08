4a49d6cf447b38fcdd65a4cd9694435680e51f8b
Joe-Mwangi <joem99271@gmail.com>
2024-06-08 11:15:50 +0300

    updated input

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { useStepsStore } from "./hooks/use-steps-store";
+import { Input } from "./ui/input";
 
 // const message = "This Field is Required";
 
@@ -12,10 +13,10 @@ const FormCard = () => {
       <div className="flex flex-col gap-4">
         <div>
           <div className="flex justify-between">
-            <label className="font-bold text-marine-blue">Name</label>
+            <label className="font-medium mb-2 text-marine-blue">Name</label>
           </div>
 
-          <input
+          <Input
             className="font-bold text-light-gray"
             placeholder="eg. Stephen King"
             type="text"
@@ -24,30 +25,39 @@ const FormCard = () => {
             onChange={updateFormValues("name")}
           />
         </div>
-        <div className="flex justify-between">
-          <label className="font-bold text-marine-blue">Email Address</label>
+        <div>
+          {" "}
+          <div className="flex justify-between">
+            <label className="font-medium mb-2 text-marine-blue">
+              Email Address
+            </label>
+          </div>
+          <Input
+            type="email"
+            name="email"
+            value={email}
+            onChange={updateFormValues("email")}
+            className="font-bold text-light-gray"
+            placeholder="eg. stephenking@lorem.com"
+          />
         </div>
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={updateFormValues("email")}
-          className="font-bold text-light-gray"
-          placeholder="eg. stephenking@lorem.com"
-        />
 
-        <div className="flex justify-between">
-          <label className="font-bold text-marine-blue">Phone Number</label>
+        <div>
+          <div className="flex justify-between">
+            <label className="font-medium mb-2 text-marine-blue">
+              Phone Number
+            </label>
+          </div>
+
+          <Input
+            type="tel"
+            value={phoneNo}
+            onChange={updateFormValues("phoneNo")}
+            className="font-bold text-light-gray"
+            placeholder="eg. +1 234 567 890"
+            name="phoneNo"
+          />
         </div>
-
-        <input
-          type="tel"
-          value={phoneNo}
-          onChange={updateFormValues("phoneNo")}
-          className="font-bold text-light-gray"
-          placeholder="eg. +1 234 567 890"
-          name="phoneNo"
-        />
       </div>
     </form>
   );
